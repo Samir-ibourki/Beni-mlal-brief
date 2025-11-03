1821040c95816e62b24efee31b7715107a98e50b
Samir-ibourki <samiribourki7@gmail.com>
2025-11-03 11:13:51 +0100

    create SplashScreen

--- app/index.jsx
+++ app/index.jsx
@@ -1,25 +1,122 @@
-//import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useEffect } from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet, Animated, Easing } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import icon from "../assets/icon.png";
+import { useRouter } from "expo-router";
 
-export default function ModalScreen() {
+export default function HomeScreen() {
+  const router = useRouter();
+  // Animated values
+  const fadeAnim = useRef(new Animated.Value(0)).current; 
+  const scaleAnim = useRef(new Animated.Value(0.8)).current;
+  const slideAnim = useRef(new Animated.Value(50)).current; 
+
+  useEffect(() => {
+
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.out(Easing.ease),
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        friction: 5,
+        useNativeDriver: true,
+      }),
+    ]).start();
+
+    
+    Animated.timing(slideAnim, {
+      toValue: 0,
+      duration: 800,
+      delay: 800,
+      easing: Easing.out(Easing.ease),
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim, scaleAnim, slideAnim]);
+
   return (
-   <View style={styles.container} >
-    <Text style={styles.text}>
-      hello world
-    </Text>
-   </View>
-  )
+    <View style={styles.container}>
+
+      <Animated.View
+        style={[
+          styles.center,
+          {
+            opacity: fadeAnim,
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
+      >
+        <Image source={icon} resizeMode="contain" style={{ width: 200, height: 200 }} />
+        <Text style={styles.text}>Beni Mellal Guide</Text>
+        <Text style={styles.subtitel}>Discover the beauty of Beni Mellal! Explore waterfalls, lakes, historic sites...</Text>
+      </Animated.View>
+
+
+      <SafeAreaView style={styles.safeBtn}>
+        <Animated.View
+          style={[
+            {
+              transform: [{ translateY: slideAnim }],
+              opacity: fadeAnim,
+              width: "100%",
+            },
+          ]}
+        >
+          <TouchableOpacity onPress={()=>router.push('/logIn')} style={styles.btn}>
+            <Text style={styles.btnText}>Log In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=>router.push('/signUp')} style={styles.btn}>
+            <Text style={styles.btnText}>Sign Up</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </SafeAreaView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  center: {
+    alignItems: "center",
+    gap: 10,
+    marginBottom:70,
   },
   text: {
-    marginTop: 15,
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  subtitel: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "#555",
+  },
+  safeBtn: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    paddingHorizontal: 20,
+    paddingBottom: 20, 
+  },
+  btn: {
+    backgroundColor: "#007BFF",
     paddingVertical: 15,
+    borderRadius: 8,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  btnText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
